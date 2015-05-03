7c3f5becb4daa1977498f3d7fe31bb6dc9a56c96
Mathieu Parent <math.parent@gmail.com>
2015-05-03 22:41:24 +0200

    Imported Upstream version 2.0.9

---
+++ Horde_Form-2.0.9/lib/Horde/Form/Translation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Form
+ *
+ * Copyright 2010-2015 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ */
+
+/**
+ * Horde_Form_Translation is the translation wrapper class for Horde_Form.
+ *
+ * @author  Jan Schneider <jan@horde.org>
+ * @package Form
+ */
+class Horde_Form_Translation extends Horde_Translation_Autodetect
+{
+    /**
+     * The translation domain
+     *
+     * @var string
+     */
+    protected static $_domain = 'Horde_Form';
+
+    /**
+     * The absolute PEAR path to the translations for the default gettext handler.
+     *
+     * @var string
+     */
+    protected static $_pearDirectory = '@data_dir@';
+}
